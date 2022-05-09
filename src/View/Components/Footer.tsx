753bf244b420e2364511473a640c36695ab07da3
Yossi A <asnafy.junior@gmail.com>
2022-05-09 09:57:46 +0300

    finishes

--- src/View/Components/Footer.tsx
+++ src/View/Components/Footer.tsx
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <div className="Footer">
             <h1>(:FULL STACK 21 TRIVIA:)</h1>
+            <h4>Copyright &copy; 2021 All rights</h4>
         </div>
     )
 }
